4fb9bb7ff6b8930f7750f7431c3f2d474f162449
goodCycle <kitiyj526@gmail.com>
2017-10-01 01:25:36 +0900

    Initial Commit

---
+++ JSON Parser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Parser
+//
+//  Created by wodl.H on 2017. 10. 1..
+//  Copyright © 2017년 desirelab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
